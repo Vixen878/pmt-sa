cdcadd1a7ff53ccd4d8928facef79cca06ba5243
Leul Zenebe <leul.zene@gmail.com>
2022-02-22 11:14:07 +0300

    admin dashboard update

--- src/pages/dashboards/admin.js
+++ src/pages/dashboards/admin.js
@@ -1,10 +1,59 @@
-
+import {UseCollection} from "../../hooks/useCollection"
 
 export default function AdminDashboard() {
+
+    // const {documents} = UseCollection()
+
     return (
-        <div>
-            info
-            {/* <button onClick={ }>Manage Account Managers</button> */}
+        <div className="flex w-full space-x-5 h-full justify-center">
+            <div className="flex flex-col">
+                <span>
+                    Total number of projects
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span>
+                    Projects waiting approval
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span>
+                    Total number of clients
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span>
+                    Completed projects
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span>
+                    Currently on going projects
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span>
+                    Cancelled projects
+                </span>
+                <span>
+                    0
+                </span>
+            </div>
         </div>
     )
 }
