002049f721d7b2fa9ab7ed563560a063012d9f74
Leul Zenebe <leul.zene@gmail.com>
2022-02-22 11:44:53 +0300

    update admin.js

--- src/pages/dashboards/admin.js
+++ src/pages/dashboards/admin.js
@@ -5,49 +5,49 @@ export default function AdminDashboard() {
     // const {documents} = UseCollection()
 
     return (
-        <div className="flex w-full space-x-5 h-full justify-center">
-            <div className="flex flex-col">
-                <span>
+        <div className="flex w-full space-x-5 h-full justify-center mt-24">
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Total number of projects
                 </span>
                 <span>
                     0
                 </span>
             </div>
-            <div className="flex flex-col">
-                <span>
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Projects waiting approval
                 </span>
                 <span>
                     0
                 </span>
             </div>
-            <div className="flex flex-col">
-                <span>
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Total number of clients
                 </span>
                 <span>
                     0
                 </span>
             </div>
-            <div className="flex flex-col">
-                <span>
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Completed projects
                 </span>
                 <span>
                     0
                 </span>
             </div>
-            <div className="flex flex-col">
-                <span>
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Currently on going projects
                 </span>
                 <span>
                     0
                 </span>
             </div>
-            <div className="flex flex-col">
-                <span>
+            <div className="flex flex-col border rounded-lg p-4 items-center">
+                <span className="text-lg font-bold text-gray-700">
                     Cancelled projects
                 </span>
                 <span>
