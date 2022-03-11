c918cda1e977734670f6914832c98926a14f325b
Minasie Shibeshi <minasploit@gmail.com>
2022-03-12 00:29:06 +0300

    added project cancellation

--- src/components/ActiveProject.js
+++ src/components/ActiveProject.js
@@ -3,16 +3,16 @@ import { UseDocument } from "../hooks/useDocument"
 import BoardWrapper from "../components/BoardWrapper"
 import { UseColumns } from "../hooks/UseColumns";
 import { UseCards } from "../hooks/UseCards";
-import { collection, doc, setDoc, onSnapshot, query, addDoc, Timestamp, orderBy, deleteDoc, updateDoc, where } from "firebase/firestore";
+import { doc, setDoc, Timestamp, deleteDoc, updateDoc, collection } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 import { v4 as uuidv4 } from 'uuid';
 import Chat from "./Chat";
 import { toast } from "react-toastify";
 
-function ActiveProject() {
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+import React from 'react'
 
+function ActiveProject() {
     const { id } = useParams()
     const { document, error } = UseDocument('projects', id)
 
@@ -73,13 +73,71 @@ function ActiveProject() {
             }
         }
 
+        async function cancelProject() {
+            try {
+                updateDoc(doc(db, "projects", id), {
+                    isCancelled: true
+                })
+
+                updateDoc(doc(db, "requests", id), {
+                    isCancelled: true
+                })
+
+                const notificationRef = doc(collection(db, "notifications"));
+                try {
+                    await setDoc(notificationRef, {
+                        content: 'Your project has been cancelled',
+                        id: notificationRef.id,
+                        is_read: false,
+                        relative_link: `/project/${id}`,
+                        user_target: document.createdBy.id
+                    });
+                } catch (err) {
+                    console.log(err.message)
+                }
+
+                toast.success("You have cancelled the project")
+            } catch (ex) {
+                toast.error(`Error ocurred: ${ex}`)
+            }
+        }
+
+        async function clearCancellationRequest() {
+            try {
+                updateDoc(doc(db, "projects", id), {
+                    cancellationRequested: false
+                })
+
+                updateDoc(doc(db, "requests", id), {
+                    cancellationRequested: false
+                })
+
+                const notificationRef = doc(collection(db, "notifications"));
+                try {
+                    await setDoc(notificationRef, {
+                        content: 'Your cancellation has been cleared',
+                        id: notificationRef.id,
+                        is_read: false,
+                        relative_link: `/project/${id}`,
+                        user_target: document.createdBy.id
+                    });
+                } catch (err) {
+                    console.log(err.message)
+                }
+
+                toast.success("You have cleared the cancellation request for this project")
+            } catch (ex) {
+                toast.error(`Error ocurred: ${ex}`)
+            }
+        }
+
         return (
             <div className="h-full">
                 <div className="flex">
                     <div className="flex w-3/4">
                         <span className='text-4xl'>{document.name}</span>
                         <div className="flex-1"></div>
-                        {!document?.isCompleted &&
+                        {!document?.isCompleted && !document.cancellationRequested &&
                             <span className="rounded border p-3 bg-primaryGreen font-semibold text-white hover:bg-green-700 cursor-pointer right"
                                 onClick={() => { if (window.confirm("Are you sure you want to mark the project as completed?")) markAsCompleted() }}>
                                 Mark as Completed
@@ -89,20 +147,41 @@ function ActiveProject() {
                 </div>
                 <div className="flex h-full">
                     <div className="w-3/4 relative pt-3">
-                        {document?.isCompleted &&
-                            <div className="absolute h-full w-full flex items-center justify-center backdrop-blur-md">
-                                <span className="text-green-700 text-2xl">Project completed</span>
-                            </div>
-                        }
-                        <BoardWrapper
-                            board={boardData}
-                            onNewCard={onNewCard}
-                            onRenameColumn={onRenameColumn}
-                            onRenameCard={onRenameCard}
-                            onRemoveCard={onRemoveCard}
-                            onColumnNew={onColumnNew}
-                            onColumnRemove={onColumnRemove}
-                            onCardDragEnd={onCardDragEnd} />
+                        {!document.cancellationRequested &&
+                            <React.Fragment>
+                                {document?.isCompleted &&
+                                    <div className="absolute h-full w-full flex items-center justify-center backdrop-blur-md">
+                                        <span className="text-green-700 text-2xl">Project completed</span>
+                                    </div>
+                                }
+                                <BoardWrapper
+                                    board={boardData}
+                                    onNewCard={onNewCard}
+                                    onRenameColumn={onRenameColumn}
+                                    onRenameCard={onRenameCard}
+                                    onRemoveCard={onRemoveCard}
+                                    onColumnNew={onColumnNew}
+                                    onColumnRemove={onColumnRemove}
+                                    onCardDragEnd={onCardDragEnd} />
+                            </React.Fragment>}
+                        {document.cancellationRequested &&
+                            <div className="flex flex-col justify-center items-center w-full h-full">
+                                <div className="text-2xl">
+                                    <span>The client has requested to cancel the project. Please select what to do...</span>
+                                </div>
+                                <div className="mt-10 flex space-x-4">
+                                    <span className="rounded border border-red-400 p-3 hover:border-2 cursor-pointer font-semibold">
+                                        <div onClick={clearCancellationRequest}>
+                                            Clear cancellation request
+                                        </div>
+                                    </span>
+                                    <span className="rounded border p-3 bg-red-500 font-semibold text-white hover:bg-red-700 cursor-pointer">
+                                        <div onClick={() => { if (window.confirm("Are you sure you want to confirm the cancel request?")) cancelProject() }}>
+                                            Cancel project
+                                        </div>
+                                    </span>
+                                </div>
+                            </div>}
                     </div>
                     <div className="w-1/4 h-full mt-[-3%]">
                         <Chat id={id} project={document} />
